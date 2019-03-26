b26534fd7ba53186af2be1ef903fcdbe4a0349d6
Piotr Głaz <piotrekglaz@icloud.com>
2019-03-26 11:29:59 +0100

    Updates

---
+++ precache-manifest.128dcec7d3067955e68051690f5effd9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "ccdec12fb3f88b3ed77c",
+    "url": "/app/static/js/runtime~main.ccdec12f.js"
+  },
+  {
+    "revision": "fd6258683e86e364e349",
+    "url": "/app/static/js/main.fd625868.chunk.js"
+  },
+  {
+    "revision": "ff83a01f97dd3367c2b3",
+    "url": "/app/static/js/2.ff83a01f.chunk.js"
+  },
+  {
+    "revision": "fd6258683e86e364e349",
+    "url": "/app/static/css/main.182e0601.chunk.css"
+  },
+  {
+    "revision": "b6991287380b4d8a640ead5fb2d7f6d1",
+    "url": "/app/index.html"
+  }
+];
